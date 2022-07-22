f66d8f16d4773788e736326b60f8e00e9a9f1525
Alec <freshalacrity@gmail.com>
2022-07-22 15:42:49 -0600

    Moved To Do items to ReadMe

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# front-tracker
+# Front Tracker
  
+## To Do
+
+### Next
+- mark unavailable component digits etc (since it's easier to split into those by tapping fronters)
+  - add resting status for digits
+
+### Then
+- style present digits by % to fit on one line
+- known issues: sort order doesn't always put present headmates first (not fixed; to reproduce )
+- search by name + digits
+- output as ?active=24,5,6 and generate a link (output to search bar?)
+- load from URL parameter
+- style: size up the present fronters to be all on one row (when possible) and add some space below that
+- show emojis
+- connect to PluralKit API
+- names off to right side with details?
+- refactor to use an object to store current, past, and future states
+
+### Later
+- ? silhouette for headmates that don't have portraits
+   - suggest picrews from headmates who share digits
+- shift click multiple portraits to merge
+- CTRL + click headmates to split; pulls up a popup that shows split options/previews the split (that disappears when CTRL is released - split to digits - or an option is clicked)
+- ??? + click headmates to mark as stable - show merges of this headmate as unavailable
+- search by emojis (use the emoji list in the spreadsheet to allow search by near miss)
+- change classes: replace present and available classes with ones for for not available + not fronting
+- toggle show/hide: 
+  - digits
+  - emojis (if portraits aren't shown, make them big?)
+  - fursona view (images + names)
+  - only current fronters
+  - only available members
+  - only registered members/members with nicknames
+- double click? to pull up details (including Picrew)
+- output possible duties to address?
+- track what front combinations happen most often
+- support undo/redo?
+- double click to open pluralkit info page
+- add background colors for images without background colors
+    - sample from images where present and add those colors to the spreadsheet + PK
--- scripts/fronting.js
+++ scripts/fronting.js
@@ -573,44 +573,6 @@ const hexVals = [
 "#C67171"  
 ];
 
-
-//      = Next =
-// mark unavailable component digits etc (since it's easier to split into those by tapping fronters)
-  // add resting status for digits
-
-//      = To Do =
-// style present digits by % to fit on one line
-// known issues: sort order doesn't always put present headmates first (fixed?)
-// search by name + digits
-// output as ?active=24,5,6 and generate a link (output to search bar?)
-// load from URL parameter
-// style: size up the present fronters to be all on one row (when possible) and add some space below that
-// show emojis
-// connect to PluralKit API
-// names off to right side with details?
-// refactor to use an object to store current, past, and future states
-
-//          = Later =
-// ? silhouette for headmates that don't have portraits
-   // suggest picrews from headmates who share digits
-// shift click multiple portraits to merge
-// CTRL + click headmates to split; pulls up a popup that shows split options/previews the split (that disappears when CTRL is released - split to digits - or an option is clicked)
-// search by emojis (use the emoji list in the spreadsheet to allow search by near miss)
-// change classes: replace present and available classes with ones for for not available + not fronting
-// toggle show/hide: 
-  // digits
-  // emojis (if portraits aren't shown, make them big?)
-  // fursona view (images + names)
-  // only fronters
-  // only available members
-// double click? to pull up details (including Picrew)
-// output possible duties to address?
-// track what front combinations happen most often
-// support undo/redo?
-// double click to open pluralkit info page
-// add background colors for images without background colors
-    // sample from images where present and add those colors to the spreadsheet + PK
-
 let baseNum = 9;
 let cap = 4; // n-fusion maximum
 let totalNum = 0;
@@ -820,6 +782,7 @@ function fillInHeadmates() {
       status: { 
         available: true, 
         present: false,
+        stable: false,
         resting: false
       }
     };
