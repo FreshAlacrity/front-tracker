b7c8954b26686f8800ad849e0bac47d8b3ee4481
Alec <freshalacrity@gmail.com>
2024-10-20 07:10:38 -0700

    Added example data

---
+++ scripts/pkData/exmpl.js
@@ -0,0 +1,52 @@
+var exampleSys = {
+  "version": 2,
+  "id": "exmpl",
+  "uuid": "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX",
+  "name": "Example System",
+  "description": "We're an example system, with sample data for your unit testing needs!",
+  "tag": "| Example System",
+  "pronouns": "they/them (plural), you&/y'all/you",
+  "avatar_url": "https://cdn.pluralkit.me/images/f3/oexkzfv7w7ii2wqgnte2ybfe.webp",
+  "banner": null,
+  "color": null,
+  "created": "2020-01-12T02:00:33.308518Z",
+  "webhook_url": null,
+  "privacy": {
+    "name_privacy": "public",
+    "avatar_privacy": "public",
+    "description_privacy": "public",
+    "pronoun_privacy": "public",
+    "member_list_privacy": "public",
+    "group_list_privacy": "public",
+    "front_privacy": "public",
+    "front_history_privacy": "public"
+  },
+  "config": {
+    "timezone": "UTC",
+    "pings_enabled": true,
+    "latch_timeout": 0,
+    "member_default_private": false,
+    "group_default_private": true,
+    "show_private_info": true,
+    "member_limit": 1000,
+    "group_limit": 250,
+    "case_sensitive_proxy_tags": false,
+    "proxy_error_message_enabled": true,
+    "hid_display_split": false,
+    "hid_display_caps": false,
+    "hid_list_padding": "off",
+    "description_templates": []
+  },
+  "accounts": [
+  ],
+  "members": [
+    {
+      "id": "myriad"
+    }
+  ],
+  "groups": [
+
+  ],
+  "switches": [
+  ]
+}
