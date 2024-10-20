4ca3ada735368e1c27835cd2c249cbe19f51bd2b
Alec <freshalacrity@gmail.com>
2024-10-20 06:33:27 -0700

    Log function tidying

--- scripts/pkData/handler.js
+++ scripts/pkData/handler.js
@@ -1,41 +1,19 @@
 // see https://github.com/FreshAlacrity/pluralkit-data-handler
 // #TODO
-// get this working here, then upload to its own repo and add back here
-// remember that how tokens etc are stored varies between here and the Butler
-// separate out the functions we use to extend members as a 'members' or 'm' sub-object?
-// also API calls
+/*
+- get this working here, then upload to its own repo and add back here as submodule
+- remember that how tokens etc are stored varies between here and the Butler
+- separate out the functions we use to extend members as a 'members' or 'm' sub-object?
+- pull PK API functions into this and make those their own sub-library
+*/
 
 const pkData = (function () {
   /* INTERNAL GLOBALS */
-  var SYSTEM_OBJECT = {}
+  var SYSTEM_STORAGE = {}
 
   /* UTILITY */
-  /**
-   * Logs things in the standard format for this library so it's clear where the error came from.
-   * @param {...*} - any number of inputs to be logged as a single entry
-   * @returns {*} the first parameter, to be used for inline debugging
-   */
-  function log(input) {
-    const logString = [...arguments].map(prettyPrint).join(' ')
-    console.log(`    pkData.log:\n\n${logString}\n\n`)
-    return input
-  }
-
-  function setLogFunction(fn) {
-    if (typeof fn == "function") {
-      log = fn;
-    } else {
-      // #todo reset to original function instead
-      log(`Param was ${typeof fn} (not a function)`)
-    }
-  }
-
-  function sayHello() {
-    log("pkData library loaded - current version: 0.1 (unstable)")
-  }
-
   /** Returns a neatly formatted string given most input; may butcher short objects containing strings with spaces. */
-  function prettyPrint(x) {
+  function pretty (x) {
     if (typeof x === 'string') {
       return x
     } else {
@@ -50,12 +28,35 @@ const pkData = (function () {
       return longString
     }
   }
+  /**
+   * Logs things in the standard format for this library so it's clear where the error came from.
+   * @param {...*} - any number of inputs to be logged as a single entry
+   * @returns {*} the first parameter, to be used for inline debugging
+   */
+  function titledLog (input) {
+    console.log(`    pkData.log:\n\n${[...arguments].map(pretty).join(' ')}\n\n`)
+    return input
+  }
+  function sayHello () { log("pkData library loaded - current version: 0.1 (unstable)") }
+  function setLogFunction (fn) {
+    if (typeof fn == "function") {
+      this.log = fn
+      log = fn // #later figure out how/why this works without setting it up as a global first
+    } else {
+      // Reset to original function
+      this.log = titledLog
+      log = titledLog
+      throw new Error(`This is not a function - ${typeof fn} ${JSON.stringify(fn)}; log function reset to default`)
+    }
+  }
+  setLogFunction(titledLog) // set the initial log function
+
 
   /* unit testing */
   const unitTests = (function () {
     const allTests = []
 
-    function add(...tests) {
+    function add (...tests) {
       allTests.push(tests)
     }
 
@@ -69,7 +70,7 @@ const pkData = (function () {
     * @param {boolean} [suppressLog=false] - Truthy values suppress logging for false tests.
     * @returns {boolean|string} - If given a testName returns a detailed test result readout, else returns a boolean value representing whether the test passed.
     */
-    function doTest(testName, setupResult, actualResult, expectedResult) {
+    function doTest (testName, setupResult, actualResult, expectedResult) {
       /* convert to strings so object comparisons work well */
       setupResult = JSON.stringify(setupResult)
       actualResult = JSON.stringify(actualResult)
@@ -102,7 +103,7 @@ const pkData = (function () {
     )
 
     /** Run a sequence of tests from an array through test() */
-    function tests(name, ...testArrays) {
+    function tests (name, ...testArrays) {
       let failed = []
       testArrays.forEach((a, i) => {
         let result = doTest('', a[0], a[1], a[2])
@@ -120,7 +121,7 @@ const pkData = (function () {
     }
 
     /** Run all the tests in the unitTests array. */
-    function runUnitTests() {
+    function runUnitTests () {
       const testResults = allTests.map(a => tests(...a))
       log('Unit Test Results:\n-------------------------\n\n' + testResults.join('\n'))
     }
@@ -138,7 +139,7 @@ const pkData = (function () {
    * @note see https://stackoverflow.com/questions/122102/what-is-the-most-efficient-way-to-deep-clone-an-object-in-javascript
    * @note Strips out properties whose values are functions or undefined, converts NaN and Infinity to null, stringifies dates, and converts regex to empty objects and otherwise messes up 'Maps, Sets, Blobs, FileLists, ImageDatas, sparse Arrays, Typed Arrays or other complex types'
    */
-  function copy(inputObj) { return JSON.parse(JSON.stringify(inputObj)) }
+  function copy (inputObj) { return JSON.parse(JSON.stringify(inputObj)) }
   // #todo does this fail for large objects?
   // #todo include in unit test a date, a regex, infinity, and a map
   unitTests.add('copy()',
@@ -154,7 +155,7 @@ const pkData = (function () {
   /* set PK data */
   /* GENERAL */
   /* global */
-  function systemDataReport(sysObj = SYSTEM_OBJECT, intro = "System Data") {
+  function systemDataReport (sysObj = SYSTEM_STORAGE, intro = "System Data") {
     return intro + ":\n" + Object.keys(sysObj).map(k => {
       if (Array.isArray(sysObj[k])) {
         return `${k}: ${sysObj[k].length} entries`
@@ -167,50 +168,50 @@ const pkData = (function () {
       }
     }).join("\n")
   }
-  function overrideSystemData(systemObj, logImport = true) {
+  function overrideSystemData (systemObj, logImport = true) {
     // If a system object was entered, use a copy of that object
     // Otherwise overwrite the system entirely
     // #later is this wise to do without requiring some sort of boolean to authorize it?
     let newObj = systemObj ? copy(systemObj) : {};
-    SYSTEM_OBJECT = newObj
-    if (logImport) { log(systemDataReport(SYSTEM_OBJECT, "System data successfully imported")) }
+    SYSTEM_STORAGE = newObj
+    if (logImport) { log(systemDataReport(SYSTEM_STORAGE, "System data successfully imported")) }
     return newObj;
   }
-  function exportSystemData() {
-    return copy(SYSTEM_OBJECT)
+  function exportSystemData () {
+    return copy(SYSTEM_STORAGE)
   }
-  function test_sysMangement() {
+  function test_sysMangement () {
     let backup = exportSystemData()
-    let initial = Object.keys(SYSTEM_OBJECT).length
+    let initial = Object.keys(SYSTEM_STORAGE).length
     overrideSystemData({ 'foo': 'bar' }, false)
-    let temp = Object.keys(SYSTEM_OBJECT).length
+    let temp = Object.keys(SYSTEM_STORAGE).length
     overrideSystemData(backup, false)
-    return (initial != temp) && (Object.keys(SYSTEM_OBJECT).length == initial)
+    return (initial != temp) && (Object.keys(SYSTEM_STORAGE).length == initial)
   }
   // #todo figure out why this fails
   unitTests.add('exportSystemData() overrideSystemData()', ["none", test_sysMangement(), true])
 
-  function setGlobal(type, data = []) {
+  function setGlobal (type, data = []) {
     log(`Setting ${type}`)
     let newData = copy(data)
-    SYSTEM_OBJECT[type] = newData;
+    SYSTEM_STORAGE[type] = newData;
     return newData;
   }
-  function getGlobal(type) {
+  function getGlobal (type) {
     log(`Getting ${type}`)
     // If the data isn't present, fetch it using an API call instead
     // #todo check if it's even a valid type here too?
-    if (!SYSTEM_OBJECT[type] || SYSTEM_OBJECT[type].length == 0) { fetchSystemData(type) }
-    return SYSTEM_OBJECT[type];
+    if (!SYSTEM_STORAGE[type] || SYSTEM_STORAGE[type].length == 0) { fetchSystemData(type) }
+    return SYSTEM_STORAGE[type];
   }
   unitTests.add('setGlobal() getGlobal()',
-    [setGlobal('foo', [1, 2]), getGlobal('foo'), [1, 2]]
+    //[setGlobal('foo', [1, 2]), getGlobal('foo'), [1, 2]]
   )
   // #todo delete foo later
 
-  function clearSystemData() {
+  function clearSystemData () {
     // #later consider what properies this ought to have
-    SYSTEM_OBJECT = {
+    SYSTEM_STORAGE = {
       "members": [],
       "groups": [],
       "switches": []
@@ -218,9 +219,9 @@ const pkData = (function () {
     //["members", "groups", "switches"].forEach(type => setGlobal(type, []));
   }
 
-  function hasId(pk, id) { return (pk.id === id || pk.uuid === id); }
+  function hasId (pk, id) { return (pk.id === id || pk.uuid === id); }
 
-  function getById(type, id) {
+  function getById (type, id) {
     if (type.slice(type.length - 1) !== 's') { type += 's' }
     let list = getGlobal(type).filter(g => hasId(g, id));
     if (list.length != 1) {
@@ -235,7 +236,7 @@ const pkData = (function () {
    * @returns {number} 1 for full match, 0 for no match
    * @author Myr
    */
-  function hasName(name, pk) {
+  function hasName (name, pk) {
     // #later also return partial match values between 0 and 1
     if (pk.name && pk.name.toLowerCase() === name.toLowerCase()) { return 1 }
     if (pk.display_name && pk.display_name.toLowerCase() === name.toLowerCase()) { return 1 }
@@ -250,7 +251,7 @@ const pkData = (function () {
    * @returns {Array<Object>} group/member objects matching the name given
    * @author Myr
    */
-  function getByName(type, name) {
+  function getByName (type, name) {
     // #later sort by match quality
     if (type.slice(type.length - 1) !== 's') { type += 's' }
     let arr = getGlobal(type);
@@ -265,7 +266,7 @@ const pkData = (function () {
   /* group functions */
   /* member list functions */
 
-  function listNames(objArr, preferDisplayName = false) {
+  function listNames (objArr, preferDisplayName = false) {
     // Mostly for debugging
     if (preferDisplayName) {
       return objArr.map(g => (g.display_name || g.name));
@@ -276,37 +277,37 @@ const pkData = (function () {
   /* member functions */
 
 
-  function getSwitches() {
+  function getSwitches () {
     // #todo #later fetch from PK and make these asynchronous?
     return getGlobal("switches");
   }
-  function getSwitch(switchNum = 0) {
+  function getSwitch (switchNum = 0) {
     // #todo make async and auto fetch older switches if the num is higher than whats currently available
     return getGlobal("switches")[switchNum];
   }
 
 
   // = MEMBERS =
-  function getFronters(switchNum = 0) {
+  function getFronters (switchNum = 0) {
     return getGroupMemberObjects(getSwitch(switchNum));
   }
-  function test_getFronters() {
+  function test_getFronters () {
     log(getFronters(2))
   }
 
 
-  function countMatching(type, obj) {
+  function countMatching (type, obj) {
     let matchBy = obj.id ? 'id' : 'uuid'
     if (!obj[matchBy]) {
-      throw new Error(`This object has no id or uuid value:\n${prettyPrint(obj)}`);
+      throw new Error(`This object has no id or uuid value:\n${pretty(obj)}`);
     }
     return getGlobal(type + "s").filter(m => m[matchBy] == obj[matchBy]).length
   }
-  function checkIfEntryExists(type, obj) {
+  function checkIfEntryExists (type, obj) {
     // #todo alert if more than 1?
     return countMatching(type, obj) == 1
   }
-  function addEntry(type, obj) {
+  function addEntry (type, obj) {
     if (type.slice(-1) != "s") { type = type + "s" }
     setGlobal(type, getGlobal(type).push(obj))
     return true
@@ -321,7 +322,7 @@ const pkData = (function () {
    * @author Penelope
    * @see {@link https://pluralkit.me/api/endpoints/#members PluralKit Endpoint Documentation}
    */
-  function updateEntryByType(type, obj) {
+  function updateEntryByType (type, obj) {
     // #todo finish and test
     // Currently only works for groups and members
     // Object must have a id or uuid to match to previous record
@@ -356,11 +357,11 @@ const pkData = (function () {
     }))
     return obj // this now includes any data that was present but not overwritten
   }
-  function updateGroupInGlobals(obj) {
+  function updateGroupInGlobals (obj) {
     return updateEntryByType('group', obj)
   }
   // #todo #later make this into a unit test and change it back after
-  function test_updateEntryByType() {
+  function test_updateEntryByType () {
     /*
     let testMember = {
       "privacy": {
@@ -391,7 +392,7 @@ const pkData = (function () {
 
   // Format any linked properties into a hyperlink
   // @author August
-  function makeLinks(obj) {
+  function makeLinks (obj) {
     let linked = Object.keys(obj).filter(h => Object.keys(obj).includes(h + " Link"));
     linked.forEach(h => {
       obj[h] = `[${obj[h]}](${obj[h + " Link"]})`;
@@ -403,7 +404,7 @@ const pkData = (function () {
   // #later rewrite (use regex?)
   // @author June
   // @author Hall
-  function objFromDescription(string, intro = "Intro") {
+  function objFromDescription (string, intro = "Intro") {
     let d = {};
     if (string) {
       let parts = string.split('\n**')
@@ -428,14 +429,14 @@ const pkData = (function () {
     d = makeLinks(d);
     return d;
   }
-  function test_objFromDescription() {
+  function test_objFromDescription () {
     pkData.clear();
     let description = getByName("member", "Hall")[0]["description"];
     log(objFromDescription(description));
   }
 
   // @author August
-  function listThese(descObj, headers = [], include = false, tail = "\n\n") {
+  function listThese (descObj, headers = [], include = false, tail = "\n\n") {
     // Returns a string with bolded headers for each of the object's properties
     // Avoid side effecting by copying the object
     let d = copy(descObj);
@@ -447,7 +448,7 @@ const pkData = (function () {
     headers = headers.filter(h => d.hasOwnProperty(h));
     return headers.map(h => `**${h}**: ${d[h]}`).join(tail);
   }
-  function discordStringFromObj(d, tail = "\n\n", intro = "Intro") {
+  function discordStringFromObj (d, tail = "\n\n", intro = "Intro") {
     let start = [intro, "Internal Name Translation", "Specific Boundaries"];
     let end = ["Music", "Music Link", "Voice Recording", "Picrew"];
     let string = [
@@ -457,14 +458,14 @@ const pkData = (function () {
     ].join(tail).slice(intro.length + 6);
     return string;
   }
-  function test_descriptionParsing() {
+  function test_descriptionParsing () {
     let initial = log(twineObj().description, "(original)");
     let recompiled = discordStringFromObj(log(objFromDescription(initial)));
     if (recompiled !== initial) { log("Something is wrong with this recompiled description:", "\n" + recompiled) }
   }
 
   // @author Myr, August
-  function getSectionFromDesc(pk, header) {
+  function getSectionFromDesc (pk, header) {
     let regex = new RegExp("\\*\\*" + header + "\\*\\*:(.+)$", "gim");
     if (!pk.description) { return "" }
     let matches = pk.description.match(regex);
@@ -473,7 +474,7 @@ const pkData = (function () {
     return matches[0].slice(('**' + header + '**:').length);
   }
   // @author Myr, August
-  function getListFromDesc(pk, header) {
+  function getListFromDesc (pk, header) {
     let str = getSectionFromDesc(pk, header);
     return (str === '') ? [] : str.split(", ").map(p => p.trim());
   }
@@ -483,24 +484,24 @@ const pkData = (function () {
    * @returns {Array<Object>} group objects for every group that member belongs to
    * @author Myr
    */
-  function getMemberGroups(id = "uioxq") {
+  function getMemberGroups (id = "uioxq") {
     let pk = getById("member", id);
     let groups = getGlobal("groups");
     return groups.filter(g => {
       return (g.members.includes(pk.id) || g.members.includes(pk.uuid))
     })
   }
-  function test_getMemberGroups() {
+  function test_getMemberGroups () {
     pkData.clear(); // so the Butler will use live data
     log(listNames(getMemberGroups()).join(", "))
   }
 
-  function getMemberAlts(pk) {
+  function getMemberAlts (pk) {
     // #todo
   }
 
   // = GROUPS =
-  function getNamedGroupId(name) {
+  function getNamedGroupId (name) {
     let dict = {
       copilots: "jxgrj",
       alt_copilots: "edoeg",
@@ -519,28 +520,28 @@ const pkData = (function () {
     }
     return groupId;
   }
-  function getNamedGroup(name) {
+  function getNamedGroup (name) {
     let groupId = getNamedGroupId(name);
     return getById("group", groupId);
   }
-  function test_GetNamedGroup() {
+  function test_GetNamedGroup () {
     //s.clear();
     log(getNamedGroup("council"))
   }
 
-  function getGroupMemberObjects(pkGroupObj) {
+  function getGroupMemberObjects (pkGroupObj) {
     return pkGroupObj.members.map(id => getById("member", id))
   }
-  function test_GetGroupMemberObjects() {
+  function test_GetGroupMemberObjects () {
     log(getGroupMemberObjects(getNamedGroup("council")))
   }
 
   /* MEMBERS */
   // @author Bryn
-  function isMember(pk, group) {
+  function isMember (pk, group) {
     return group["members"].includes(pk.id) || group["members"].includes(pk.uuid);
   }
-  function test_isMember() {
+  function test_isMember () {
     pkData.clear(); // clear the export data to use live data only
     let pk = getByName("member", "June")[0];
     let groupA = getById("group", "iyusd")
@@ -551,27 +552,27 @@ const pkData = (function () {
     log(`${pk.name} is in ${groupB.name}: ${isMember(pk, groupB)}`)
   }
 
-  function regex(type) {
+  function regex (type) {
     let key = {
       'isUtility': '[A-Z]'
     }
     return new RegExp(key[type])
   }
-  function isUtility(pk) {
+  function isUtility (pk) {
     let text = getCompactSetString(pk)
     return regex('isUtility').test(text)
   }
-  function isAlt(pk) {
+  function isAlt (pk) {
     let text = getCompactSetString(pk)
     let regex = new RegExp('[\'"]')
     return regex.test(text)
   }
-  function isPrimary(pk) {
+  function isPrimary (pk) {
     return (!isUtility(pk) && !isAlt(pk))
   }
   /*
   // Known issue: workflow for unit tests includes clobbering the system global before it gets here (should be fixed)
-  function runMemberTypeTest(k) {
+  function runMemberTypeTest (k) {
     let pk = getById('member', k) // note: this will only work after the lib is initiated
     return [getNickname, isUtility, isAlt, isPrimary].map(f => f(pk))
   }
@@ -586,21 +587,17 @@ const pkData = (function () {
   });
   */
 
-  /* ??? */
-  function test_this() {
-    return this
-  }
-
-
   /* Reveal functions and set aliases visible to outside scopes. */
   return {
+    /* PKDATA LIB FUNCTIONS */
     // Note: if changing any of the property names, leave a note about what it used to be
     // Make sure everything here has a unit test please ^^
 
     /* UTILITY */
+    'log': log,
+    'logWith': setLogFunction,
     'hello': sayHello,
-    'runUnitTests': unitTests.run,
-    'setLogFunction': setLogFunction,
+    'prettyPrint': pretty,
     /* GENERAL */
     'init': overrideSystemData,
     'clear': clearSystemData,
@@ -622,7 +619,6 @@ const pkData = (function () {
     'objectFromName': getByName, // params: type, name
     'objectFromId': getById,
     /* TEMP */
-    'test': test_this
   }
 }())
 
@@ -635,9 +631,9 @@ const objFromDescription = pkData.headerListToObj
 const getMemberGroups = pkData.getMemberGroups
 const clearSystemData = pkData.clear
 const getGlobal = pkData.listAll
-const getGroups = pkData.listAll.bind(sys, "groups")
+const getGroups = pkData.listAll.bind(pkData, "groups")
 const setGlobal = pkData.updateTypeList
-const updateMemberInGlobals = pkData.updateEntry.bind(sys, "member") // first param is the 'this' value
+const updateMemberInGlobals = pkData.updateEntry.bind(pkData, "member") // first param is the 'this' value
 const updateGroupInGlobals = pkData.updateGroup
 const hasEntry = pkData.hasEntry
 const getSwitches = pkData.switches
